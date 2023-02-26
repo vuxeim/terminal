8e949fbea79b780f258056796abe1115472a9861
vuxeim <35169070+vuxeim@users.noreply.github.com>
2023-02-26 18:58:31 +0100

    update url, links behavior

--- links.js
+++ links.js
@@ -1,1 +1,15 @@
-document.querySelectorAll(".navigation>ul>*").forEach(e => (e.nodeName == 'LI') && e.addEventListener("click", () => navigator.clipboard.writeText(e.attributes.hover.value)));
+document.querySelectorAll(".navigation>ul>*")
+.forEach(e => (e.nodeName == 'LI')
+    && e.addEventListener("click", () =>
+        (e.attributes.not_url
+        && navigator.clipboard.writeText(e.attributes.hover.value))
+        || window.open("https://"+e.attributes.hover.value, "_blank")
+    )
+);
+
+
+
+// document.querySelectorAll(".navigation>ul>*")
+// .forEach(e => (e.nodeName == 'LI') && e.attributes.not_url
+//     && e.addEventListener("click", () =>
+//         navigator.clipboard.writeText(e.attributes.hover.value)));
